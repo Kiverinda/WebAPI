6cb7f989bde6a7b30420507272f48ae296f9f3c8
Kiverinda <67836207+Kiverinda@users.noreply.github.com>
2021-03-20 20:29:38 +0500

    Update MetricsRepository.cs

--- MetricsManager/MetricsRepository.cs
+++ MetricsManager/MetricsRepository.cs
@@ -5,26 +5,26 @@ namespace MetricsManager
 {
     public class MetricsRepository
     {
-        public List<Metrics> metrics { get; set; }
+        private List<Metrics> _metrics;
 
         public MetricsRepository() 
         {
-            metrics = new List<Metrics>();
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-12T12:11:12"), 12));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-13T12:11:12"), 15));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-13T11:11:12"), 3));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-14T15:11:12"), -6));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-14T16:11:12"), 2));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-14T17:11:12"), 8));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-14T18:11:12"), 9));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-15T12:11:12"), -5));
-            metrics.Add(new Metrics(DateTime.Parse("2021-12-15T13:11:12"), -13));
+            _metrics = new List<Metrics>();
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-12T12:11:12"), 12));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-13T12:11:12"), 15));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-13T11:11:12"), 3));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-14T15:11:12"), -6));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-14T16:11:12"), 2));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-14T17:11:12"), 8));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-14T18:11:12"), 9));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-15T12:11:12"), -5));
+            _metrics.Add(new Metrics(DateTime.Parse("2021-12-15T13:11:12"), -13));
         }
 
         public List<Metrics> Read(DateTime fromDate, DateTime toDate)
         {
             List<Metrics> list = new List<Metrics>();
-            foreach(Metrics m in metrics)
+            foreach(Metrics m in _metrics)
             {
                 if (m.date >= fromDate && m.date <= toDate) list.Add(m);
             }
@@ -33,16 +33,16 @@ namespace MetricsManager
 
         public void Add(DateTime date, int temperature)
         {
-            metrics.Add(new Metrics(date, temperature));
+            _metrics.Add(new Metrics(date, temperature));
         }
 
         public void Delete(DateTime fromDate, DateTime toDate)
         {
-            for (int i = 0; i < metrics.Count; i++)
+            for (int i = 0; i < _metrics.Count; i++)
             {
-                if (metrics[i].date >= fromDate && metrics[i].date <= toDate)
+                if (_metrics[i].date >= fromDate && _metrics[i].date <= toDate)
                 {
-                    metrics.RemoveAt(i);
+                    _metrics.RemoveAt(i);
                     i--;
                 }
             }
@@ -50,7 +50,7 @@ namespace MetricsManager
 
         public void Update(DateTime date, int temperature)
         {
-            foreach (Metrics m in metrics)
+            foreach (Metrics m in _metrics)
             {
                 if (m.date == date) m.temperature = temperature;
             }
